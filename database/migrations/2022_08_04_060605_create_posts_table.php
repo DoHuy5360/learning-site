90230420d9fe0a2a69e1f02f24b0b1c0c6ff7e17
Dohuy5360 <loldh321@gmail.com>
2022-08-05 23:00:13 +0700

    save process: comment field

--- database/migrations/2022_08_04_060605_create_posts_table.php
+++ database/migrations/2022_08_04_060605_create_posts_table.php
@@ -15,13 +15,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('creator'); // Sua lai thanh interger
             $table->string('title');
             $table->text('content');
-            $table->integer('view')->default(0);
             $table->string('time', 50)->nullable();
             $table->string('made', 10)->nullable();
-            $table->timestamps();
         });
     }
 
